73caf19d0ce21fbfb6b37a1adeb97bf51d7cb61c
Emmanuel Wanjala <starrise1440@gmail.com>
2026-09-07 08:39:08 +0300

    Configure dynamic base path for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
+   // Uses '/' on Vercel and '/my-portfolio/' on GitHub Pages
+  base: process.env.VERCEL ? '/' : '/my-portfolio/',
 })
+
+
+
+
